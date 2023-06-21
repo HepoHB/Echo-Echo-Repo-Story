885bc099c100fe6728c85df4060c001123868817
HepoHB <NeroHaziel@outlook.com>
2023-06-21 17:30:29 -0300

    SQLAlchemy Tests

---
+++ Python/SQLAlchemy/main.py
@@ -0,0 +1,38 @@
+import sqlalchemy as SQL
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "User"
+    cd_id = SQL.Column(SQL.Integer, primary_key=True)
+    nm_name = SQL.Column(SQL.String)
+    nm_fullname = SQL.Column(SQL.String)
+
+    adress = relationship(
+        "Adress", back_populates="User", cascade="all, delete-orphan"
+
+    )
+
+    def __repr__(self):
+        return f"User(id={self.cd_id}, name={self.nm_name}, fullname={self.nm_fullname})"
+
+
+class Address(Base):
+    __tablename__ = "Address"
+    cd_id = SQL.Column(SQL.Integer, primary_key=True)
+    nm_mail = SQL.Column(SQL.String(50), nullable=False)
+    cd_userId = SQL.Column(SQL.Integer, SQL.ForeignKey("User.cd_id"))
+
+    user = relationship(
+        "User", back_populates="Address"
+        
+    )
+
+    def __repr__(self):
+        return f"Address(id={self.cd_id}, e-mail={self.nm_mail})"
+
+
+print(User.nm_name)
